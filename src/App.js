b3805e7bdc795e27cea85c3a1d44f5d877cc5894
Arturo Bravo Rovirosa <arturo123leyenda@gmail.com>
2019-12-30 21:34:39 -0600

    render props and like button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyle } from './styles/GlobalStyles'
-import {ListOfPhotoCards} from './container/ListOfPhotoCards'
-import {Logo} from './components/Logo'
+import { ListOfPhotoCards } from './container/ListOfPhotoCards'
+import { Logo } from './components/Logo'
+import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
 
-export const App = () => (
-  <div>
-    <Logo/>
-    <GlobalStyle />
-    <ListOfCategories />
+export const App = () => {
+  const urlParams = new window.URLSearchParams(window.location.search)
+  const detailId = urlParams.get('detail')
 
-    <ListOfPhotoCards categoryId={1} />
-  </div>
-)
+  return (
+    <div>
+      <GlobalStyle />
+      <Logo />
+      {
+        detailId
+          ? <PhotoCardWithQuery id={detailId} />
+          : <Fragment>
+            <ListOfCategories />
+            <ListOfPhotoCards categoryId={1} />
+          </Fragment>
+      }
+    </div>
+  )
+}
